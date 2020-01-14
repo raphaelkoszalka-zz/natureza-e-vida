bc28c65a15c03394be9049f289fcf7df6a674f8c
Raphael Luiz Martins Koszalka <rmkoszalka@gmail.com>
2020-01-14 16:43:45 -0300

    MainComponent with landing page initial view

--- src/components/main/MainComponent.js
+++ src/components/main/MainComponent.js
@@ -8,6 +8,15 @@ class MainComponent extends Component {
         return (
           <section>
             <HeaderComponent />
+            <div class="container main-text">
+              <div class="col-xs-12 col-md-6">
+                <h1>
+                  Vivamus <strong>efficitur</strong> justo lectus, <br />
+                  ac luctus <strong>risus</strong> suscipit a.
+                </h1>
+                <button class="btn btn-lg">Venha nos visitar!</button>
+              </div>
+            </div>
           </section>
         );
     }
