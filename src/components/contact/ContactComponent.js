aa8377542c4ffe61612a1f5dfd8d10b34f8f16b9
Raphael Luiz Martins Koszalka <rmkoszalka@gmail.com>
2020-01-20 20:08:06 -0300

    GoogleMaps

--- src/components/contact/ContactComponent.js
+++ src/components/contact/ContactComponent.js
@@ -7,7 +7,7 @@ class ContactComponent extends Component {
     render() {
         return (
           <div id="contact-component">
-            <div className="col-md-6">
+            <div className="col-md-6 App">
               <MapComponent />
             </div>
             <div className="col-md-6">
@@ -16,26 +16,26 @@ class ContactComponent extends Component {
               </h1>
               <form className="padding-top">
                 <div className="form-group">
-                  <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Email" />
+                  <input type="email" className="form-control" id="exampleInputEmail1" placeholder="Email" />
                 </div>
                 <div className="form-group">
-                  <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Assunto" />
+                  <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Assunto" />
                 </div>
                 <div className="form-group">
-                  <input type="text" class="form-control" id="exampleInputPassword1" placeholder="Nome" />
+                  <input type="text" className="form-control" id="exampleInputPassword1" placeholder="Nome" />
                 </div>
                 <div className="form-group">
-                  <input type="text" class="form-control" id="exampleInputPassword1" placeholder="WhatsApp" />
+                  <input type="text" className="form-control" id="exampleInputPassword1" placeholder="WhatsApp" />
                 </div>
                 <div className="form-group">
-                  <input type="text" class="form-control" id="exampleInputPassword2" placeholder="Mensagem" />
+                  <input type="text" className="form-control" id="exampleInputPassword2" placeholder="Mensagem" />
                 </div>
               </form>
               <div className="clearfix"></div>
               <br />
               <br />
               <br />
-              <button type="submit" class="btn btn-default contact-button">Enviar</button>
+              <button type="submit" className="btn btn-default contact-button">Enviar</button>
             </div>
           </div>
         );
